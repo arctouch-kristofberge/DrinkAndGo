8bb0074ef97c128a818de02e5c4ace6021e72163
Andre Sato <andre.sato@arctouch.com>
2018-01-11 20:19:20 -0200

    add session delegate code

--- iOS/AR/ARViewSessionDelegate.cs
+++ iOS/AR/ARViewSessionDelegate.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using ARKit;
+
 namespace DrinkAndGo.iOS.AR
 {
-	public class ARViewSessionDelegate
+	public class ARViewSessionDelegate : ARSessionDelegate
 	{
 		public ARViewSessionDelegate()
 		{
+		}
+
+		public override void CameraDidChangeTrackingState(ARSession session, ARCamera camera)
+		{
+			Console.WriteLine("{0} {1}", camera.TrackingState, camera.TrackingStateReason);
 		}
 	}
 }
