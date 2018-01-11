702ed0b16fb2221029e27d08dd2ac8a519bdfb9c
Kristof Berge <kristof.berge@arctouch.com>
2018-01-11 18:36:23 -0200

    Create project

---
+++ DrinkAndGo/DrinkAndGoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace DrinkAndGo
+{
+    public partial class DrinkAndGoPage : ContentPage
+    {
+        public DrinkAndGoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
